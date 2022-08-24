f4acd8b474d9b85bbab423f310fdb2362e4067e0
Vivek Vajpai <vivekvajpai65@gmail.com>
2022-08-24 19:35:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+        <td><%= link_to 'Destroy', article, method: :delete, data: { confirm: 'Are you sure?' } %></td>
+
+<%= link_to user.username, user_path(user) %>
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+	before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+	def show
+		@articles = @user.articles
+	end
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			# session[:user_id] = @user_id
+			flash[:notice] = "#{@user.username}, You have successfully signed up."
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Your account information was successfully updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
+    end
+  end
+
+end
